180b298c15535447eab281fc3f76bda71a571fb4
Memnoc <smartdroidesign@gmail.com>
2018-08-03 09:53:41 +0100

    test

--- age_script.js
+++ age_script.js
@@ -2,7 +2,7 @@ var age = prompt("How old are you?");
 
 
 function calculateAge(age) {
-	var days = age * 365.25;
+	var days = age * 363.25;
 	alert("You have been alive for " + days + " days.")
 };
 
