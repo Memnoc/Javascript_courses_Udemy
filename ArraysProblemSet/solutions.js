90b99dc1cb8eda397f25dba14bb0cc8c41ae7217
Memnoc <smartdroidesign@gmail.com>
2018-08-04 12:16:21 +0100

    printReverse problem-solved

---
+++ ArraysProblemSet/solutions.js
@@ -0,0 +1,24 @@
+// printReverse
+// Write a function printReverse() that takes an arrays as an argument and prints out the elements in the array
+// in reverse order (don't actually rever the array itself);
+
+console.log("****************---printReverse---*************************")
+
+let myArray = [1,2,3,4,5,6,7,8,9,10];
+
+function printReverse(arr) {
+    arr.reverse();
+        console.log(arr);
+    }
+
+    function printReverse2(arr) {
+        let reverse = arr.length-1;
+        for(let i = reverse; i >=0; i--) {
+            console.log(arr[i]);
+        }
+    }
+
+printReverse(myArray);
+printReverse2(myArray);
+
+console.log("****************---end---*************************")
