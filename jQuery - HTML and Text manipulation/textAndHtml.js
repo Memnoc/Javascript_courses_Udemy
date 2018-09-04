a6fa95a5b332b78a19aaf67798d837709ed84b0a
Memnoc <smartdroidesign@gmail.com>
2018-09-04 15:58:57 +0100

    text manipualtion

---
+++ jQuery - HTML and Text manipulation/textAndHtml.js
@@ -0,0 +1,10 @@
+// Manipulate Text
+$("h1").text("New Text");
+
+// Manipulate Multiple Text entries
+$('li').text("Rusty, Colt's dog is adorable");
+
+$('ul').html("<li>I Hacked your UL!</li>");
+
+$('li').html("<a href='google.com'>CLICK ME TO GO TO GOOGLE</a>");
+
