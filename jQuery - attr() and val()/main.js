eb63ffef4a45f9de134d3d8a8c01b4f7f6b337d1
Memnoc <smartdroidesign@gmail.com>
2018-09-04 16:22:04 +0100

    attr() method

---
+++ jQuery - attr() and val()/main.js
@@ -0,0 +1,18 @@
+// Change image size
+$('img').css("width", "500px");
+
+// Retrieve the image
+$('img').attr("src");
+
+// Change the image 
+$('img').attr("src", "https://i.ytimg.com/vi/zx0gXM_FkgE/maxresdefault.jpg");
+
+// Update only the first image
+$('img:first').attr("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM3Ai2LAZurR0Oyho4tgLhnt74n2EhrnlQXirn9H7dbuiZM_WL");
+
+// Update only the last image
+$("img").last().attr("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM3Ai2LAZurR0Oyho4tgLhnt74n2EhrnlQXirn9H7dbuiZM_WL");
+
+// Update all 3 images
+
+$("img").attr("src", "https://www.vwt.org.uk/wp-content/uploads/2015/03/Pine-marten-BANNER.jpg");
