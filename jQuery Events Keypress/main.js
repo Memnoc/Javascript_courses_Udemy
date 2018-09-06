9749544ab1cb4106d34a409ac5971e1e53f85e55
Memnoc <smartdroidesign@gmail.com>
2018-09-06 09:16:07 +0100

    keypress and "which"

--- jQuery Events Keypress/main.js
+++ jQuery Events Keypress/main.js
@@ -8,3 +8,18 @@ console.log.apply(console, $("input"));
 $("input").keypress(function(){
 	console.log("You have pressed a key");
 });
+
+// Record for an event in console
+$("input").keypress(function(event){
+	console.log(event);
+});
+
+// Every event has code: https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode
+// We'll capture the one for the enter key
+// "which" specifies the key reading the code when pressed
+
+$("input").keypress(function(event){
+	if(event.which === 13){
+		console.log("You hit ENTER!");
+	}
+});
