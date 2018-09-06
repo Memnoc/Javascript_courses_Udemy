d67d2e16522b2a281aea920850ca03aeaf72a269
Memnoc <smartdroidesign@gmail.com>
2018-09-06 14:19:24 +0100

    todos functionalities completed

---
+++ TO-DO-LIST/assets/js/todos.js
@@ -0,0 +1,23 @@
+// Make an LI grey upon lick and adda a strik-through effect
+$("ul").on("click", "li", function () {
+    $(this).toggleClass("completed");
+});
+
+// Remove the li when clicking X
+$("ul").on("click", "span", function(event){
+        $(this).parent().fadeOut(500, function(){
+            $(this).remove();
+        });
+        event.stopPropagation();
+    });
+
+    $("input[type='text']").keypress(function(event){
+        if(event.which === 13) {
+            // Grabbing new todo text from input
+            var todoText = $(this).val();
+            $(this).val("");
+            // Create a new li and add to ul
+            $("ul").append("<li><span>X</span> " + todoText + "</li>");
+        }
+        
+    });
