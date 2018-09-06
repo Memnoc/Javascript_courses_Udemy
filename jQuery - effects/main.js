8b6f4c51e12836d1ae58c4353207bd142ef9699b
Memnoc <smartdroidesign@gmail.com>
2018-09-06 10:15:35 +0100

    small adjustments

--- jQuery - effects/main.js
+++ jQuery - effects/main.js
@@ -1,30 +1,30 @@
 // Fadeout effect - like this, it only hides the elements
-// $("button").on("click", function(){
-//     $("div").fadeOut("slow", function(){
-//         console.log("Fade Out");
-//     });
-// });
+$("button").on("click", function(){
+    $("div").fadeOut("slow", function(){
+        console.log("Fade Out");
+    });
+});
 
-// // You can remove them completely
-// $("button").on("click", function(){
-//     $("div").fadeOut("slow", function(){
-//         $(this).remove();
-//     });
-// });
+// You can remove them completely
+$("button").on("click", function(){
+    $("div").fadeOut("slow", function(){
+        $(this).remove();
+    });
+});
 
-// // Fade in
-// $("button").on("click", function(){
-//     $("div").fadeIn("slow", function(){
-//         console.log("Fade In");
-//     });
-// });
+// Fade in
+$("button").on("click", function(){
+    $("div").fadeIn("slow", function(){
+        console.log("Fade In");
+    });
+});
 
-// // Fade toggle
-// $("button").on("click", function(){
-//     $("div").fadeToggle("slow", function(){
-//         console.log("Fade Toggle");
-//     });
-// });
+// Fade toggle
+$("button").on("click", function(){
+    $("div").fadeToggle("slow", function(){
+        console.log("Fade Toggle");
+    });
+});
 
 //// Sliding effects /////
 
