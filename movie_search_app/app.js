490168e98cc393f3482da2386109475e6d0759b7
Memnoc <smartdroidesign@gmail.com>
2018-09-18 16:28:17 +0100

    API's Search form

---
+++ movie_search_app/app.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("search");
+});
+
+app.get("/results", function(req, res){
+    var query = req.query.search;
+    var url = "http://www.omdbapi.com/?i=tt3896198&apikey=8882f317&s=" + query;
+    request(url, function(error, response, body){
+        if(!error && response.statusCode == 200) {
+            var data =JSON.parse(body);
+            res.render("results", {data: data});
+        }
+    })
+})
+
+
+app.listen(3000, function(){
+    console.log("Server has started");
+});
+
+// Search with Movie ID: http://www.omdbapi.com/?i=tt3896198&apikey=thewdb
+
+// request('http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb');
